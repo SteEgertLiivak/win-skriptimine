4676ef82fae1c749f3eaa400738fa9f2b7e60aca
Sten Egert Liivak <sten-egert.liivak@voco.ee>
2022-11-08 13:59:53 +0200

    parameetrid yl

---
+++ parameetrid.ps1
@@ -0,0 +1,7 @@
+﻿param(
+$kasutajaNimi,
+$taisNimi,
+$kontoKirjeldus
+)
+$KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+New-LocalUser $kasutajaNimi -Password $kasutajaParool -FullName $taisNimi -Description $kontoKirjeldus

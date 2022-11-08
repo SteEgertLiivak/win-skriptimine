3c9f253f475e30492bf04bb38f9da74d1df56a75
Sten Egert Liivak <sten-egert.liivak@voco.ee>
2022-11-08 02:03:45 -0800

    lokaalse kasutaja loomine windows serveris

---
+++ kasutaja_loomine_lokaalne.ps1
@@ -0,0 +1,4 @@
+﻿#määrame kasutajale parool, konverteerides turvatud tekstis
+ConvertTo-SecureString "Parool1!" -AsPlainText -force
+#loome kasutaja
+new-localuser "kasutaja" -Password $kasutajaparool -fullname "esimene tavakasutaja" -Description "Local Account - kasutaja"

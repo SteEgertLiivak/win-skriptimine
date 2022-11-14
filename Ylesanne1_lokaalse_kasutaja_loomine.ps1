25186345bc87ae950b051a7c2ede95df5f0cd46d
Sten Egert Liivak <sten-egert.liivak@voco.ee>
2022-11-14 10:03:08 +0200

    lokaalse kasutaja loomine

---
+++ Ylesanne1_lokaalse_kasutaja_loomine.ps1
@@ -0,0 +1,19 @@
+﻿#küsime kasutaja ees- ja perenime ja teeme need lowercase (olenemata, mis sisestatakse)
+$eesnimi = read-host "Sisestage oma eesnimi (ainult ladinatähtedega)"
+$eesnimi = $eesnimi.ToLower()
+$perenimi = read-host "Sisestage oma perenimi (ainult ladinatähtedega)"
+$perenimi = $perenimi.ToLower()
+#annan kasutajale parooliks Parool1! securestringina
+$password = ConvertTo-SecureString "Parool1!" -AsPlainText -force
+new-localuser "$eesnimi.$perenimi" -Password $password -fullname "$eesnimi.$perenimi" -Description "Lokaalne kasutaja"
+#eemaldan errori teatised, kui samanimeline user on juba olemas
+if($?)
+{   
+echo "Kasutaja loomine õnnestus!"
+
+}
+else
+{
+$ErrorActionPreference = 'SilentlyContinue'
+echo "Kasutaja loomisel tekkinud viga"
+}

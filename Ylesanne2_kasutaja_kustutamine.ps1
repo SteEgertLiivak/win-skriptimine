5328db9e21553994affdb264b62922ef52a05eea
Sten Egert Liivak <sten-egert.liivak@voco.ee>
2022-11-16 10:45:36 +0200

    lokaalse kasutaja kustutamine

---
+++ Ylesanne2_kasutaja_kustutamine.ps1
@@ -0,0 +1,18 @@
+﻿#küsime kasutaja ees- ja perenime
+$eesnimi = read-host "Sisestage kasutaja eesnimi: "
+$eesnimi = $eesnimi
+$perenimi = read-host "Sisestage kasutaja perenimi: "
+$perenimi = $perenimi
+#annan kasutajale parooliks Parool1! securestringina
+Remove-LocalUser -Name "$eesnimi $perenimi"
+#eemaldan errori teatised, kui kasutajat ei eksisteeri
+if($?)
+{   
+echo "Kasutaja on edukalt kustutatud."
+
+}
+else
+{
+$ErrorActionPreference = 'SilentlyContinue'
+echo "Kasutaja ei eksisteeri või kustutamisel tekkis muu viga, proovige uuesti."
+}
